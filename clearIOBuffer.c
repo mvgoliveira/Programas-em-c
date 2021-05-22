8f96cde02b688dc5b590331851f51ff4b8225889
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2021-05-22 20:49:04 -0300

    ✨ Added Replace /n for /0 function

--- clearIOBuffer.c
+++ clearIOBuffer.c
@@ -6,6 +6,8 @@ void clearBuffer(char nome[]) {
   if(!strchr(nome, '\n'))
     while((getchar())!='\n');
   nome[strcspn(nome,"\n")] = 0;
+
+  nome[strcspn (nome, "\n")] = 0; // Para substituir o /n por /0 (Nada)
 }
 
 int main(){
